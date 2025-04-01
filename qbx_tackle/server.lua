c56ac33c18252289520a5b2c18583dd4fc4bc40a
Arfy <aalouane@gmail.com>
2025-04-01 20:44:22 +0000

    disable tackle + stun effect and afk time changed + crouch key changed to U

--- qbx_tackle/server.lua
+++ qbx_tackle/server.lua
@@ -1,10 +1,10 @@
-RegisterNetEvent('tackle:server:TacklePlayer', function(target)
-    local src = source
+-- RegisterNetEvent('tackle:server:TacklePlayer', function(target)
+--     local src = source
     
-    local srcCoords = GetEntityCoords(GetPlayerPed(src))
-    local targetCoords = GetEntityCoords(GetPlayerPed(target))
+--     local srcCoords = GetEntityCoords(GetPlayerPed(src))
+--     local targetCoords = GetEntityCoords(GetPlayerPed(target))
 
-    if #(srcCoords - targetCoords) > 2.0 then return end
+--     if #(srcCoords - targetCoords) > 2.0 then return end
 
-    TriggerClientEvent('tackle:client:GetTackled', target)
-end)
+--     TriggerClientEvent('tackle:client:GetTackled', target)
+-- end)
